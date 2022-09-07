241b469a49af5e6236f4b68963b936cc6e5ac3fb
Jack <Jwenschhof@bsd220.org>
2022-09-07 10:09:34 -0500

    Polishing designs with fonts and colors

--- iDine/ItemRow.swift
+++ iDine/ItemRow.swift
@@ -8,15 +8,32 @@
 import SwiftUI
 
 struct ItemRow: View {
+    let colors: [String: Color] = ["D": .purple, "G": .black, "N": .red, "S": .blue, "V": .green]
+    
     let item: MenuItem
     
     var body: some View {
         HStack {
             Image(item.thumbnailImage)
+                .clipShape(Circle())
+                .overlay(Circle().stroke(Color.gray,lineWidth: 2))
             
             VStack(alignment: .leading) {
                 Text(item.name)
+                    .font(.headline)
                 Text("$\(item.price)")
+            }
+            
+            Spacer()
+            
+            ForEach(item.restrictions, id: \.self) { restrictions in
+                Text(restrictions)
+                    .font(.caption)
+                    .fontWeight(.black)
+                    .padding(5)
+                    .background(colors[restrictions,default: .black])
+                    .clipShape(Circle())
+                    .foregroundColor(.white)
             }
         }
     }
